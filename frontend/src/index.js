9501e7356064f740a2e2af0825a0891229614789
Feroz-NearTekPod <ferozkhan.zabiulla@neartekpod.com>
2025-05-30 16:39:54 +0530

    Remove unused `authInterceptor` and `reportWebVitals` files.
    - Cleaned up `index.js` by removing references to deprecated features.
    - Streamlined initialization process by eliminating redundant imports.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { KeycloakProvider } from './auth/KeycloakProvider';
-// Kept for compatibility but no longer does anything
-import { setupAuthInterceptors } from './services/authInterceptor';
-
-// Set up is kept for compatibility
-setupAuthInterceptors();
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,8 +10,3 @@ root.render(
         <App />
     </KeycloakProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
